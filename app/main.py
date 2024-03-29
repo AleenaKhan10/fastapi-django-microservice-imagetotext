fae59b2e493ead1af2a5cc8f6c929c77cd8e6e9b
AleenaKhan10 <aleenakhanraees40@gmail.com>
2024-03-29 11:52:05 +0500

    adding the pytest and written few test cases for two basic  endpoints.

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 @app.get("/", response_class=HTMLResponse)
 def home_view(request: Request):
-    return templates.TemplateResponse("home.html", {"request":request, "abc" : 123})
+    return templates.TemplateResponse(request, "home.html", {"abc": 123})
 
 @app.post("/")
 def home_detail_view():
